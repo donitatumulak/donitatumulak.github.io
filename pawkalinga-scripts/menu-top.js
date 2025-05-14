93c74047b54281298b52a1e0aef751845b2f3657
donidonita <tumulakdonita@gmail.com>
2025-05-14 10:35:39 +0800

    Update menu-top.js

--- pawkalinga-scripts/menu-top.js
+++ pawkalinga-scripts/menu-top.js
@@ -22,3 +22,34 @@ document.addEventListener("DOMContentLoaded", function() {
   backToTopButton.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sectionsToAnimate = [
+    document.getElementById("browseIntro"),
+    document.getElementById("browseIntro"),
+    document.querySelector(".adoption-steps"),
+    document.querySelector(".browse-step"),
+    document.querySelector(".section-introduction"),
+    document.querySelector(".about-us"),        
+    document.querySelector(".our-mission"),
+    document.querySelector(".faq-section")
+  ];
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+  );
+
+  sectionsToAnimate.forEach((section) => {
+    if (section) observer.observe(section);
+  });
+});
